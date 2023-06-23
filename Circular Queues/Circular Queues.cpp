97927883f2369d51cecfdd6e5b754741564e846b
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-06-23 19:42:44 +0700

    ew

--- Circular Queues/Circular Queues.cpp
+++ Circular Queues/Circular Queues.cpp
@@ -46,6 +46,11 @@ public:
 		}
 		cout << "\nThe element deleted from the queue is: " << queue_array[front] << "\n";
 
+		//cek jika antrian hanya memiliki satu elemen
+		if (front == rear) {
+			front = -1;
+			rear = -1;
+		}
 
 	}
 
