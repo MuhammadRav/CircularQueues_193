1a664c4dd99322f59225088f873594d10cf869c1
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-06-23 19:54:40 +0700

    iy

--- Circular Queues/Circular Queues.cpp
+++ Circular Queues/Circular Queues.cpp
@@ -107,6 +107,9 @@ int main() {
 		try {
 			cout << "Menu" << endl;
 			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
 		}
 	}
 }
